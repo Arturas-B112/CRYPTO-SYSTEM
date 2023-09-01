3c21ee08d7f234f5880fd5c962f22d5da9b74491
Arturas Borodko <arturas.borodko@gmail.com>
2023-09-01 14:52:44 +0300

    Created dropdown search with nav to single coin

--- web/src/pages/crypto-monitoring-page/CryptoMonitoringPage.jsx
+++ web/src/pages/crypto-monitoring-page/CryptoMonitoringPage.jsx
@@ -1,11 +1,16 @@
 import { useEffect, useState } from 'react';
 import { getCrypto } from '../../api';
 import CryptoTable from './CryptoTable';
-import { LinearProgress } from '@mui/material';
+import { Autocomplete, LinearProgress, Stack, TextField } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 
 const CryptoMonitoringPage = () => {
   const [currencies, setCurrencies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [inputValue, setInputValue] = useState('');
+  const [errorText, setErrorText] = useState('');
+
+  const navigate = useNavigate();
 
   const fetchCurrencies = async () => {
     try {
@@ -25,10 +30,56 @@ const CryptoMonitoringPage = () => {
     fetchCurrencies();
   }, []);
 
+  const handleInputChange = (e, newValue) => {
+    if (newValue.length <= 30) {
+      setInputValue(newValue);
+    }
+
+    if (
+      !currencies.some((currency) =>
+        currency.name.toLowerCase().includes(newValue.toLowerCase())
+      )
+    ) {
+      setErrorText('No matching options found.');
+    } else {
+      setErrorText('');
+    }
+  };
+
   return (
     <>
+      <Stack direction="row" gap={1} mx={2}>
+        <Autocomplete
+          freeSolo
+          options={currencies}
+          getOptionLabel={(currency) => currency.name}
+          onChange={(e, currency) => {
+            if (currency) {
+              navigate(`/coins/${currency.id}`, { state: { currency } });
+            }
+          }}
+          inputValue={inputValue}
+          onInputChange={handleInputChange}
+          sx={{ width: 300 }}
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              label="Search..."
+              variant="standard"
+              error={!!errorText}
+              helperText={errorText}
+              inputProps={{ ...params.inputProps, maxLength: 30 }}
+            />
+          )}
+        />
+      </Stack>
       {isLoading && <LinearProgress />}
-      <CryptoTable currencies={currencies} />
+      <CryptoTable
+        currencies={currencies}
+        onDetails={(currency) => {
+          navigate(`/coins/${currency.id}`, { state: { currency } });
+        }}
+      />
     </>
   );
 };
--- web/src/pages/crypto-monitoring-page/CryptoTable.jsx
+++ web/src/pages/crypto-monitoring-page/CryptoTable.jsx
@@ -1,26 +1,30 @@
 import { Button } from '@mui/material';
 import './CryptoTable.scss';
 
-const CryptoTable = ({ currencies }) => {
+const CryptoTable = ({ currencies, onDetails }) => {
   return (
     <>
       <table>
-        <tr>
-          <th>#</th>
-          <th>Name</th>
-          <th>Price</th>
-          <th>Actions</th>
-        </tr>
-        {currencies.map((currencie) => (
-          <tr key={currencie.id}>
-            <td>{currencie.market_cap_rank}</td>
-            <td>{` ${currencie.name} / ${currencie.symbol}`}</td>
-            <td>{currencie.current_price}</td>
-            <td>
-              <Button>Details</Button>
-            </td>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Name</th>
+            <th>Price</th>
+            <th>Actions</th>
           </tr>
-        ))}
+        </thead>
+        <tbody>
+          {currencies.map((currency) => (
+            <tr key={currency.id}>
+              <td>{currency.market_cap_rank}</td>
+              <td>{` ${currency.name} / ${currency.symbol}`}</td>
+              <td>{`$ ${currency.current_price}`}</td>
+              <td>
+                <Button onClick={() => onDetails(currency)}>Details</Button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
       </table>
     </>
   );
