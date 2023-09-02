1273525395156ca4afad3d0edcb88acef84c21fe
Arturas Borodko <arturas.borodko@gmail.com>
2023-09-02 14:38:45 +0300

    Created first single coin page section

--- web/src/api/index.js
+++ web/src/api/index.js
@@ -9,3 +9,6 @@ export const getTrendingCoins = (currency) =>
   axios.get(
     `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=gecko_desc&per_page=10&page=1&sparkline=false&price_change_percentage=24h`
   );
+
+export const getSingleCoin = (id) =>
+  axios.get(`https://api.coingecko.com/api/v3/coins/${id}`);
--- web/src/pages/single-coin-page/SingleCoinPage.jsx
+++ web/src/pages/single-coin-page/SingleCoinPage.jsx
@@ -1,12 +1,70 @@
 import { useParams } from 'react-router-dom';
+import { CryptoState } from '../../CryptoContext';
+import { getSingleCoin } from '../../api';
+import { LinearProgress, Typography } from '@mui/material';
+import { useEffect, useState } from 'react';
+import { Stack } from '@mui/material';
+import CurrencySelector from '../../components/currency-selector/CurrencySelector';
 
 const SingleCoinPage = () => {
   const { id } = useParams();
+  const [coin, setCoin] = useState();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const { currency, symbol } = CryptoState();
+
+  const fetchSingleCoin = async (id) => {
+    try {
+      setIsLoading(true);
+
+      const { data } = await getSingleCoin(id);
+
+      setCoin(data);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchSingleCoin(id);
+  }, []);
 
   return (
     <>
-      <h1>Dummy</h1>
-      {console.log(id)}
+      <Stack mx={2} alignItems="flex-end">
+        <CurrencySelector />
+      </Stack>
+
+      {isLoading && <LinearProgress />}
+      <Stack direction={{ xs: 'column', md: 'row' }} my={2} mx={2}>
+        <Stack alignItems="center" spacing={2}>
+          <img
+            src={coin?.image.large}
+            alt="Coin logo"
+            style={{ width: '50%' }}
+          />
+          <Typography variant="h4">{coin?.name}</Typography>
+          <Stack>
+            <Typography variant="h5">
+              Current Price:{' '}
+              {coin?.market_data.current_price[currency.toLowerCase()]} {symbol}
+            </Typography>
+            <Typography variant="h5">
+              Market Cap:{' '}
+              {coin?.market_data.market_cap[currency.toLowerCase()]
+                .toString()
+                .slice(0, -6)}
+              M {symbol}
+            </Typography>
+            <Typography variant="h5">
+              Total Supply: {coin?.market_data.total_supply.toFixed(2)}{' '}
+              {coin?.symbol}
+            </Typography>
+          </Stack>
+        </Stack>
+      </Stack>
     </>
   );
 };
