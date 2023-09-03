a2a786b9c952f7e1efab619316670eb6813bc432
Arturas Borodko <arturas.borodko@gmail.com>
2023-09-03 15:40:46 +0300

    Created backend app to save/log user actions

--- server/index.js
+++ server/index.js
@@ -1,13 +1,58 @@
 const express = require('express');
 const cors = require('cors');
-const ccxt = require('ccxt');
 require('dotenv').config();
+const { MongoClient } = require('mongodb');
 
 const server = express();
 
 server.use(express.json());
 server.use(cors());
 
-server.listen(process.env.PORT, () =>
-  console.log(`Server is running on port: ${process.env.PORT}`)
-);
+const mongoClient = new MongoClient(process.env.CONNECTION_STRING);
+const PORT = process.env.PORT || 8080;
+
+server.post('/selected', async (req, res) => {
+  const payload = req.body;
+  try {
+    const mongoCluster = await mongoClient.connect();
+    const selectedCoin = {
+      name: payload.name,
+    };
+
+    const response = await mongoCluster
+      .db('CryptoSystem')
+      .collection('Actions')
+      .insertOne(selectedCoin);
+
+    await mongoCluster.close();
+    console.log(`User selected the ${payload.name}`);
+    res.status(201).json(response);
+  } catch (error) {
+    console.log(error);
+    res.status(500).end();
+  }
+});
+
+server.post('/searched', async (req, res) => {
+  const payload = req.body;
+  try {
+    const mongoCluster = await mongoClient.connect();
+    const searhedCoin = {
+      name: payload.name,
+    };
+
+    const response = await mongoCluster
+      .db('CryptoSystem')
+      .collection('Searches')
+      .insertOne(searhedCoin);
+
+    await mongoCluster.close();
+    console.log(`User searched for ${payload.name}`);
+    res.status(201).json(response);
+  } catch (error) {
+    console.log(error);
+    res.status(500).end();
+  }
+});
+
+server.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
